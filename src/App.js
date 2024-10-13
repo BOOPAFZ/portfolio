3edcd012542b88b67f1646685db82296b749afd2
BOOPAFZ <bakhsoussa@gmail.com>
2024-10-13 00:22:52 -0400

    work in progress

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Nav from './components/Nav';
+import HomePage from './components/HomePage';
+import Contact from './components/ContactMe';
+import Footer from './components/Footer';
+import Education from './components/Education';
+import Portfolio from './components/Portfolio';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="container">
+        <Header />
+        <Nav />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/About" element={<AboutMe />} />
+          <Route path="/education" element={<Education />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/contact" element={<ContactMe />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
